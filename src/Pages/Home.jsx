1a926b5d789c5ee23aafbddeb4ef1922459875cf
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-04-30 13:48:49 -0700

    Trying for CSS

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,16 +11,16 @@ function Home({setPage}) {
       <Header />
       <section className="home-card">
        <h2>Welcome Back   !</h2>
-       <Card  children=" " title="Stress" className="Stress"/>
-       <Card  children=" " title="Time Management + Homework" className="Time Management + Homework"/>
-       <Card  children=" " title="Sleep" className="Sleep"/>
+       <Card  children="Stress" title="Stress" className="Stress"/>
+       <Card  children="Time Management + Homework" title="Time Management + Homework" className="Time Management + Homework"/>
+       <Card  children="Sleep" title="Sleep" className="Sleep"/>
       </section>
       <section className="home-buttons">
-        <Button text="View To-Do list" onClick={() => setPage('To-Do')} className="BigButton"/>
-        <Button text="View Mental State" onClick={() => setPage('Mental')} className="BigButton"/>
-        <Button text="View Profile" onClick={() => setPage('Profile')} className="BigButton"/>
-        <Button text="View Settings" onClick={() => setPage('Settings')} className="BigButton"/>
-        <Button text="Log out" onClick={() => setPage('LoginPage')} className="SmallButton"/>
+        <Button text="View To-Do list" onClick={() => setPage('To-Do')} />
+        <Button text="View Mental State" onClick={() => setPage('Mental')} />
+        <Button text="View Profile" onClick={() => setPage('Profile')} />
+        <Button text="View Settings" onClick={() => setPage('Settings')} />
+        <Button text="Log out" onClick={() => setPage('LoginPage')} />
       </section>
     </main>
     </>
