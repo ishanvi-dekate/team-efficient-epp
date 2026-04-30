4ff79dd66b98f3ea03361cc024d1817ae2b9adc8
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-04-30 13:10:20 -0700

    fix nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import Nav from "./Components/Nav.jsx";
 import Home from "./Pages/Home.jsx";
 import Login from "./Components/Login.jsx";
 import Settings from "./Pages/Settings.jsx";
+import LoginPage from "./Pages/Loginpage.jsx";
 
 function App() {
   const [page, setPage] = useState("LoginPage");
 
   // Pages that should show the Nav menu (after login)
-  const showNav = page !== "LoginPage" && page !== "Login";
+  const showNav = page !== "LoginPage" && page !== "Login" && page !== "Home"; 
 
     return (
         <>
-      {showNav && <Nav setPage={setPage} />}
+      
       {page === "LoginPage" && <LoginPage setPage={setPage} />}
       {page === "Login" && <Login setPage={setPage} />}
       {page === "Home" && <Home setPage={setPage} />}
       {page === "Settings" && <Settings setPage={setPage} />}
+      {showNav && <Nav setPage={setPage} />}
         </>
     );
 }
