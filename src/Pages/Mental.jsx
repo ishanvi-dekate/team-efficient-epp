40dfb43b8f79027044ea432e456d66dd9f36e4c1
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-05-01 13:39:36 -0700

    mental page mostly done

--- src/Pages/Mental.jsx
+++ src/Pages/Mental.jsx
@@ -11,8 +11,21 @@ function Mental(){
     <Header />
     </header>
       <div className="mental-banner">
-        <h1 className="settings-title">Mental Check</h1>
+        <h1 className="mental-title">Mental Check</h1>
+      </div>
+      <div className="mental-content">
+        <p>Please fill out</p>
+      <Card  title={"What time did you sleep yesterday?"} className="mental-card"/>
+      <input className="mental-input" placeholder="Click to enter"></input>
+      <Card title={"Scale on a 1-10, how is your day?"}className="mental-card"/>
+      <input className="mental-input" placeholder="Click to enter"></input>
+      <Card title={"When did you wake up?"} className="mental-card"/>
+      <input className="mental-input" placeholder="Click to enter"></input>
+      <Card title={"List out your worries"} className="mental-card"/>
+      <input className="mental-input" placeholder="Click to enter"></input>
       </div>
       </div></>)
 }
 export default Mental
+
+ 
