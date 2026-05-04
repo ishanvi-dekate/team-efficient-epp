fb3ac128f1b2cb31523f075165b13014a0575f2d
aKarthik25 <karthika2870@my.sduhsd.net>
2026-05-04 13:32:13 -0700

    Created planner.jsx

---
+++ src/Components/Planner.jsx
@@ -0,0 +1,9 @@
+import useState from 'react'
+function Planner() {
+    <main className='weekly-planner'>
+        <h2> Weekly Planner</h2>
+        <section className='dayPlan'>
+            <h2> day</h2>
+        </section>
+    </main>
+}
