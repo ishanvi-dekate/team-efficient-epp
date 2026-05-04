ba8fc7a57878e52cdc7cf8478b027e8315959f1e
Nathan-Zhao0806 <zhaon2273@my.sduhsd.net>
2026-05-04 13:24:09 -0700

    Profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -6,7 +6,22 @@ function Profile(){
     <Header />
      <div className="profile-page">
       <div className="profile-banner">
-        <h1 className="profiless-title">Settings</h1>
+        <h2 className="profile-title">Profile</h2>
+        <h6 className="profile-username">        </h6>
+      </div>
+      <div className="profile-content">
+        <div className="profile-content-grade">Grade:    </div>
+        <div className="profile-content-aboutyou">About you:   </div>
+        <div className="profile-content-current">Current courses
+        <div className="profile-content-current-info">           ;           ;         ;          ;</div>
+        </div>
+        <div className="profile-content-current">Extracurriculars
+        <div className="profile-content-current-info">          ;           ;          ;          ;          ;          ;</div>
+        </div>
+        <div className="profile-content-current">Personal goals
+        <div className="profile-content-current-info">          ;         ;         ;</div>
+        </div>
+
       </div>
       </div>
 
