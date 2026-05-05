411e36b71fb0757d1176e90ac18d416028ec7bd9
aKarthik25 <karthika2870@my.sduhsd.net>
2026-05-05 12:37:56 -0700

    Good

--- src/Components/Planner.jsx
+++
@@ -1,9 +0,0 @@
-import useState from 'react'
-function Planner() {
-    <main className='weekly-planner'>
-        <h2> Weekly Planner</h2>
-        <section className='dayPlan'>
-            <h2> day</h2>
-        </section>
-    </main>
-}
