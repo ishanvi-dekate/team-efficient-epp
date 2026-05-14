8892203e123982dd755a38c21c2bd69ac1f7e876
aKarthik25 <karthika2870@my.sduhsd.net>
2026-05-14 20:44:29 +0000

    ViewMental

--- src/Pages/ViewMental.jsx
+++ src/Pages/ViewMental.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react"
+import Mental from "./Mental.jsx"
 function ViewMental() {
     const [page,setPage] = useState('')
     return (
         <div className="mental-page">
             <div className="mental-banner">
-                <button onClick ></button>
+                <button className= "viewMental-back-btn" onClick={()=>setPage("Mental")}>Back</button>
                 <h1 className="mental-title">View Past Mental Checks</h1>
+            </div>
+            <div className = "viewPast">
+                <table>
+                    <tr>
+                        Date
+                    </tr>
+                    <tr>
+                        Past Mental Checks
+                    </tr>
+                </table>
             </div>
 
         </div>
